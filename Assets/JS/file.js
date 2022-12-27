ecc226f3d2d3cf10e2460db864b85776d4f1d944
Palma Iacobelli <palmaiacobelli92@gmail.com>
2022-12-27 12:38:11 +0100

    avanzamento

--- Assets/JS/file.js
+++ Assets/JS/file.js
@@ -1,16 +1,20 @@
 /* const gestisciKeyUpSuInput = (event) => {
   console.log("L'utente ha digitato:", event.key);
 }; */
+window.onload = () => {
+  /* const AggiungiTask = document.getElementById("bottone");
+  AggiungiTask.addEventListener("onclick", (event) => {
+    const nuovoDiv = document.createElement("div");
+    nuovoDiv.classList.add("contenitore");
+    nuovoDiv.innerHTML = valore.InputField;
+    document.getElementsByClassName("contenitore").appendChild(nuovoDiv);
+    console.log(AggiungiTask);
+    nuovoDiv.onclick = AggiungiTask;
+  });
+};
+ */
 
-const AggiungiTask = document.getElementById("bottone");
-AggiungiTask.addEventListener("onclick", (event) => {
-  const nuovoDiv = document.createElement("div");
-  nuovoDiv.classList.add("contenitore");
-  nuovoDiv.innerHTML = valore.InputField;
-  document.getElementsByClassName("contenitore").appendChild(nuovoDiv);
-  console.log(AggiungiTask);
-  nuovoDiv.onclick = AggiungiTask;
-});
-const eliminaSelezionati = () => {
-  const list = document.get;
+  const contenitoreLista = document.getElementById("lista-items");
+  contenitoreLista.innerHTML += "<div class="lista-items">Task 1</div>";
+  contenitoreLista.innerHTML += "<div>"Task 2"</div>";
 };
