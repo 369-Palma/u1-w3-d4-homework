f35c019c6ba09131e719936fc4c15968c8ba9f24
Palma Iacobelli <palmaiacobelli92@gmail.com>
2022-12-28 13:34:52 +0100

    minor adjustment

--- Assets/JS/file.js
+++ Assets/JS/file.js
@@ -1,8 +1,6 @@
 /* const gestisciKeyUpSuInput = (event) => {
   console.log("L'utente ha digitato:", event.key);
-}; */
-window.onload = () => {
-  /* const AggiungiTask = document.getElementById("bottone");
+}; */ /* const AggiungiTask = document.getElementById("bottone");
   AggiungiTask.addEventListener("onclick", (event) => {
     const nuovoDiv = document.createElement("div");
     nuovoDiv.classList.add("contenitore");
@@ -13,11 +11,15 @@ window.onload = () => {
   });
 };
  */
-};
+window.onload = () => {};
 const contenitoreLista = document.getElementById("lista-items");
-contenitoreLista.innerHTML += "<div class=`lista-items`> Task 1 </div>";
-contenitoreLista.innerHTML += "<div class=`lista-items`> Task 2 </div>";
-
+contenitoreLista.innerHTML +=
+  "<div class=`lista-items`onclick=`nuovaClasse`(event)> Task 1 </div>";
+contenitoreLista.innerHTML +=
+  "<div class=`lista-items`onclick=`nuovaClasse`(event)> Task 2 </div>";
+const nuovaClasse = (event) => {
+  console.log(event.target);
+};
 const tastoEnter = (event) => {
   //console.log("L'ultente ha digitato:", event.key);
   if (event.key === "Enter") {
@@ -28,7 +30,9 @@ const tastoEnter = (event) => {
       const newElementoLista = document.createElement("div");
       newElementoLista.classList.add("lista-items");
       contenitoreLista.innerText = valoreField;
+      newElementoLista.onclick = nuovaClasse;
       document.getElementById("lista-items").appendChild(newElementoLista);
+      document.getElementById("Aggiungi").value = "";
     }
   }
 };
