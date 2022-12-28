dda8a93065ba495e4a82e37f2888be49a590f64a
Palma Iacobelli <palmaiacobelli92@gmail.com>
2022-12-28 12:26:41 +0100

    fatto tasto resposive alla chiave enter

--- Assets/JS/file.js
+++ Assets/JS/file.js
@@ -13,8 +13,22 @@ window.onload = () => {
   });
 };
  */
+};
+const contenitoreLista = document.getElementById("lista-items");
+contenitoreLista.innerHTML += "<div class=`lista-items`> Task 1 </div>";
+contenitoreLista.innerHTML += "<div class=`lista-items`> Task 2 </div>";
 
-  const contenitoreLista = document.getElementById("lista-items");
-  contenitoreLista.innerHTML += "<div class=`lista-items`> Task 1 </div>";
-  contenitoreLista.innerHTML += "<div class=`lista-items`> Task 2 </div>";
+const tastoEnter = (event) => {
+  //console.log("L'ultente ha digitato:", event.key);
+  if (event.key === "Enter") {
+    const valoreField = document.getElementById("Aggiungi").value;
+    if (valoreField === "") {
+      alert("Non lascire vuoto questo campo");
+    } else {
+      const newElementoLista = document.createElement("div");
+      newElementoLista.classList.add("lista-items");
+      contenitoreLista.innerText = valoreField;
+      document.getElementById("lista-items").appendChild(newElementoLista);
+    }
+  }
 };
