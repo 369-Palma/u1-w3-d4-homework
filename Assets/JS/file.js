7d9768580f803a4a90aff610ed8ee9d90860e0ef
Palma Iacobelli <palmaiacobelli92@gmail.com>
2022-12-28 19:03:24 +0100

    fanculo

--- Assets/JS/file.js
+++ Assets/JS/file.js
@@ -11,7 +11,7 @@
   });
 };
  */
-window.onload = () => {};
+/* window.onload = () => {};
 const contenitoreLista = document.getElementById("lista-items");
 contenitoreLista.innerHTML +=
   "<div class=`lista-items`onclick=`nuovaClasse`(event)> Task 1 </div>";
@@ -20,7 +20,7 @@ contenitoreLista.innerHTML +=
 const nuovaClasse = (event) => {
   console.log(event.target);
 };
-const tastoEnter = (event) => {
+const nuovaTask = (event) => {
   //console.log("L'ultente ha digitato:", event.key);
   if (event.key === "Enter") {
     const valoreField = document.getElementById("Aggiungi").value;
@@ -36,3 +36,58 @@ const tastoEnter = (event) => {
     }
   }
 };
+ */
+
+const nuovaTask = (event) => {
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("bottone");
+  let testo = document.createTextNode(inputValue);
+  li.appendChild(testo);
+  if (inputValue === "") {
+    alert("Non lasciare questo campo vuoto");
+  } else {
+    document.getElementById("myList").appendChild(li);
+  }
+
+  document.getElementById("bottone").value = "";
+
+  const span = document.createElement("SPAN");
+  const text = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(text);
+  li.appendChild(span);
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    };
+  }
+};
+
+const miaLista = document.getElementById("myList");
+for (let i = 0; i < miaLista.length; i++) {
+  let bottoncino = document.createElement("button");
+  let txt = document.createTextNode("\u00D7");
+  bottoncino.className = "close";
+  bottoncino.appendChild(txt);
+  miaLista[i].appendChild(bottoncino);
+}
+
+const close = document.getElementsByClassName("close");
+for (let i = 0; i < miaLista.length; i++) {
+  close[i].onclick = function () {
+    let div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+const lista = document.getElementById("myList");
+lista.addEventListener(
+  `click`,
+  function (evento) {
+    if (evento.target.tagName === "LI") {
+      evento.target.classList.toggle("fatto");
+    }
+  },
+  false
+);
